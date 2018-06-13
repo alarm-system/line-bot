4d2ce9cdb4bd9b22ff5068af2d2a9f22cdbbcab3
alarm-system <40227657+alarm-system@users.noreply.github.com>
2018-06-13 13:48:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'cnEhQ3b4hk242v8ClGFcw+8UqSagnYNmizjjGebkP3IaQPudbxRC2zc7PwSilDH+K5yBQtRY3A6AjZGCS6lTPAt8IGGLg8nUhbLW/jTX2ovYLErulBB+sTY/eigyD1cFvLQO0EiDyhRsp6IWQQRxWwdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
