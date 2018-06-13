c2a7a5626ef608ecaaddac4c2eb723a306b562bb
alarm-system <40227657+alarm-system@users.noreply.github.com>
2018-06-13 13:36:06 +0700

    Update webhooks.php
    change token access

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'cnEhQ3b4hk242v8ClGFcw+8UqSagnYNmizjjGebkP3IaQPudbxRC2zc7PwSilDH+K5yBQtRY3A6AjZGCS6lTPAt8IGGLg8nUhbLW/jTX2ovYLErulBB+sTY/eigyD1cFvLQO0EiDyhRsp6IWQQRxWwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
